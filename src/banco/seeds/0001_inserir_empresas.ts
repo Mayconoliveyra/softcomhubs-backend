f8a351636de1442b9e030aa9899a6513d7ad091b
Maycon Oliveira <mayconbrito1998@hotmail.com>
2025-03-18 09:44:39 -0300

    up

--- src/banco/seeds/0001_inserir_empresas.ts
+++ src/banco/seeds/0001_inserir_empresas.ts
@@ -65,6 +65,24 @@ export const seed = async (knex: Knex) => {
         sh_empresa_id: 1,
         sh_forma_pagamento: 'ESPÉCIE',
       },
+      {
+        uuid: '573fd949-ec72-4f57-858f-0f448a6f4afd',
+        registro: '59248',
+        razao: 'ELETROSUL',
+        fantasia: 'ELETROSUL LTDA ME',
+        cnpj_cpf: '28037094000192',
+        sinc_nome: false,
+        sinc_preco: true,
+        sinc_preco_custo: true,
+        sinc_preco_tipo: 'PADRAO',
+        sinc_estoque: true,
+        sinc_fabricante: true,
+        ativo: true,
+
+        sh_usuario_id: 1,
+        sh_empresa_id: 1,
+        sh_forma_pagamento: 'ESPÉCIE',
+      },
     ])
     .then(() => {
       console.log(`# Inserido dados na tabela ${ETableNames.empresas}`);
