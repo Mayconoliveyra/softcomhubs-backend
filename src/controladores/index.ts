263a2d093917da976e88af0bde2ba1c0a9e8becf
Maycon Oliveira <mayconbrito1998@hotmail.com>
2025-03-23 10:39:57 -0300

    up

--- src/controladores/index.ts
+++ src/controladores/index.ts
@@ -1,4 +1,5 @@
 import * as empresa from './empresa';
+import * as plug4market from './plug4market';
 import * as usuario from './usuario';
 
-export const Controladores = { ...usuario, ...empresa };
+export const Controladores = { ...usuario, ...empresa, ...plug4market };
